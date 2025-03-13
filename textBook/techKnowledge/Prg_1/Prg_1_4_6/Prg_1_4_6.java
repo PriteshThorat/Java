d6b2dac96a3e3b9350fdf5085d60a910aa06c12e
Pritesh Thorat <141409170+PriteshThorat@users.noreply.github.com>
2025-03-13 12:28:37 +0000

    Some Changes

--- textBook/techKnowledge/Prg_1/Prg_1_4_6/Prg_1_4_6.java
+++ textBook/techKnowledge/Prg_1/Prg_1_4_6/Prg_1_4_6.java
@@ -3,39 +3,13 @@ import java.applet.*;
 
 /*
 <applet
-<<<<<<< HEAD
     code="Prg_1_4_6"
     width="500"
     height="500" >
-=======
-  code="Prg_1_4_6"
-  width="500"
-  height="500" >
->>>>>>> 5a59265 (Some Changes)
 </applet>
 */
 
 public class Prg_1_4_6 extends Applet{
-<<<<<<< HEAD
-    public void init(){
-        List ls1 = new List(4);
-        List ls2 = new List(5, true);
-
-        ls1.add("India");
-        ls1.add("China");
-        ls1.add("Australia");
-        ls1.add("America");
-        ls2.add("Andhra Pradesh");
-        ls2.add("Karnataka");
-        ls2.add("Gujarat");
-        ls2.add("Maharashtra");
-        ls2.add("Bihar");
-        ls2.add("J & K");
-        add(ls1);
-        add(ls2);
-    }
-}
-=======
   public void init(){
     List ls1 = new List(4);
     List ls2 = new List(5, true);
@@ -54,4 +28,3 @@ public class Prg_1_4_6 extends Applet{
     add(ls2);
   }
 };
->>>>>>> 5a59265 (Some Changes)
--- textBook/techKnowledge/Prg_1/Prg_1_4_7/Prg_1_4_7.java
+++ textBook/techKnowledge/Prg_1/Prg_1_4_7/Prg_1_4_7.java
@@ -3,20 +3,13 @@ import java.applet.*;
 
 /*
 <applet
-<<<<<<< HEAD
     code="Prg_1_4_7"
     width="500"
     height="500" >
-=======
-  code="Prg_1_4_7"
-  width="500"
-  heigth="500" >
->>>>>>> 5a59265 (Some Changes)
-</applet>
+</applet
 */
 
 public class Prg_1_4_7 extends Applet{
-<<<<<<< HEAD
     public void init(){
         Scrollbar sb1 = new Scrollbar(Scrollbar.VERTICAL, 0, 1, 0, 50);
         Scrollbar sb2 = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, 0, 50);
@@ -24,10 +17,4 @@ public class Prg_1_4_7 extends Applet{
         add(sb1);
         add(sb2);
     }
-}
-=======
-  public void init(){
-    Scrollbar sb1 = new Scrollbar(Scrollbar.VERTICAL)
-  }
 };
->>>>>>> 5a59265 (Some Changes)
