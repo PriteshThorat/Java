5d2689ff683fe32b850921479de17210d38b745a
Pritesh Thorat <thoratpritesh1@gmail.com>
2025-06-01 19:46:41 +0530

    Some Changes

--- Demo/A.java
+++ Demo/A.java
@@ -1,14 +1,42 @@
-import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+class Level1 {
+    public void display(){
+        System.out.println("Calling Level1 Class display()");
+    }
+};
+
+class Level2 extends Level1 {
+    @Override
+    public void display(){
+        System.out.println("Calling Level2 Class display()");
+    }
+};
+
+class Level3 extends Level2 {
+    @Override 
+    public void display(){
+        System.out.println("Calling Level3 Class display()");
+    }
+
+    public void show(){
+        System.out.println("Calling Level3 Class show()");
+    }
+};
 
 public class A {
     public static void main(String[] args) {
-        JFrame f = new JFrame("Demo");
+        Level1 obj1 = new Level1();
+        obj1.display();
 
-        JButton btn = new JButton("Click Me");
-        btn.setToolTipText("This button does something important");
+        Level1 obj2 = new Level2();
+        obj2.display();
 
-        f.add(btn);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
+        Level1 obj3 = new Level3();
+        obj3.display();
+        
+        Level3 obj4 = new Level3();
+        obj4.show();
     }
-}
+};
