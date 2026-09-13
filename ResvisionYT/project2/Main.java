999ec94eba177a4b5ac9d00978d568328e16e5e3
Pritesh Thorat <thoratpritesh1@gmail.com>
2026-09-13 17:54:20 +0530

    3 projects added

---
+++ ResvisionYT/project2/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String args[]) {
+    System.out.print("The sum of these numbers is: ");
+
+    int num1 = 5;
+    int num2 = 10;
+    int num3 = 15;
+    int sum = num1 + num2 + num3;
+    
+    System.out.print(sum);
+  }
+}
---
+++ ResvisionYT/project3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main (String args[]) {
+    byte age = 34;
+    int age2 = 56;
+    short age3 = 87;
+    long ageDino = 333333333333333l;
+    char ch = 'A';
+    float f1 = 5.6f;
+    double d1 = 6.8;
+    boolean a = true;
+    String str = "Harry";
+  }
+}
