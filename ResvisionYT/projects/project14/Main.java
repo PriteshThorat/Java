02c9dc032b427d8ab53d5003342c090333cd273c
Pritesh Thorat <thoratpritesh1@gmail.com>
2026-09-17 19:08:14 +0530

    project 14 added

---
+++ ResvisionYT/projects/project14/Main.java
@@ -0,0 +1,39 @@
+class Base {
+  int x;
+
+  public int getX () {
+    return x;
+  }
+  public void setX (int x) {
+    this.x = x;
+  }
+  public void printMe () {
+    System.out.println("I am an construtor");
+  }
+}
+
+class Derived extends Base {
+  int y;
+
+  public int getY () {
+    return y;
+  }
+  public void setY (int y) {
+    this.y = y;
+  }
+}
+
+public class Main {
+  public static void main (String[] args) {
+    Base b = new Base();
+
+    b.setX(4);
+    System.out.println(b.getX());
+
+    Derived d = new Derived();
+    d.setX(6);
+    System.out.println(d.getX());
+    d.setY(9);
+    System.out.println(d.getY());
+  }
+}
