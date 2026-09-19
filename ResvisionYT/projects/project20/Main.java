7861323a78007b02416a2774f6a1a5cc6d4d8e04
Pritesh Thorat <thoratpritesh1@gmail.com>
2026-09-19 13:16:19 +0530

    project 20 added

---
+++ ResvisionYT/projects/project20/Main.java
@@ -0,0 +1,28 @@
+interface Bicycle {
+  public void applyBrake (int dec);
+  public void speedUp (int inc);
+}
+
+class AvonCycle implements Bicycle {
+  int speed = 7;
+
+  @Override 
+  public void applyBrake (int dec) {
+    speed -= dec;
+  }
+  @Override 
+  public void speedUp (int inc) {
+    speed += inc;
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    AvonCycle avonCycle = new AvonCycle();
+    
+    avonCycle.applyBrake(2);
+    System.out.println(avonCycle.speed);
+    avonCycle.speedUp(5);
+    System.out.println(avonCycle.speed);
+  }
+}
