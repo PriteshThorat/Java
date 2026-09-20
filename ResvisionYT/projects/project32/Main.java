34da37912284136c90a11f4c74fe58b11818d097
Pritesh Thorat <thoratpritesh1@gmail.com>
2026-09-20 16:57:57 +0530

    project 32 added

---
+++ ResvisionYT/projects/project32/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int k;
+    k = sc.nextInt();
+    System.out.println("Integer part 1000 is divided by k is " + 1000/k);
+  }
+}
