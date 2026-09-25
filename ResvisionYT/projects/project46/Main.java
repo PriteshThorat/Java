47fd2cb8a96888e433a6c1e6174d5c4781955ce9
Pritesh Thorat <thoratpritesh1@gmail.com>
2026-09-25 19:24:23 +0530

    project 46 added

---
+++ ResvisionYT/projects/project46/Main.java
@@ -0,0 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.LocalDate;
+
+public class Main {
+  public static void main(String[] args) {
+    LocalDate localDate = LocalDate.now();
+    System.out.println(localDate);
+
+    LocalTime localTime = LocalTime.now();
+    System.out.println(localTime);
+
+    LocalDateTime localDateTime = LocalDateTime.now();
+    System.out.println(localDateTime);
+  }
+}
